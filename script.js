dc9f4c2fb38a4f3e5928d2abc89b4467d4a7a13c
Karthik Sunkari <84893245+Karthiksunkari3@users.noreply.github.com>
2024-07-27 12:22:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 // complete the given function
 
 function palindrome(str){
-
+	let s="";
+    for(let i=0;i<str.length;i++)
+		{
+			s=str.charAt(i)+s;
+		}
+	if(str.equalsTo(s))
+	{
+		return true;
+	}
+	return false;
 }
 module.exports = palindrome
